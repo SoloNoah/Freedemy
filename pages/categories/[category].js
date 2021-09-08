c7bc40ec374f0d88fd9f811a3bf8170dbda38f73
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-09-08 12:39:11 +0300

    rendering free courses for each of the categories

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -3,14 +3,18 @@ import Head from 'next/head';
 import Card from '../../components/card/Card';
 import { getAllCourses } from '../../category-data/category';
 
-export default function Category({ category }) {
-  console.log('here');
+export default function Category({ category, courses }) {
   return (
     <div>
       <Head>
         <title>Freedemy | {category}</title>
       </Head>
       <h1>Path: {category}</h1>
+      <ul>
+        {courses.map((course) => {
+          return <Card course={course} key={course.id} />;
+        })}
+      </ul>
     </div>
   );
 }
@@ -30,9 +34,24 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  const BASE_URL = 'https://www.udemy.com/api-2.0/';
+  const searchParams = new URLSearchParams({
+    price: 'price-free',
+    page_size: 100,
+    category: params.category,
+  });
+  const headers = {
+    Accept: 'application/json, text/plain, */*',
+    Authorization: `Basic ${process.env.AUTH}`,
+    ContentType: 'application/json;charset=utf-8',
+  };
+  const data = await fetch(BASE_URL + 'courses?' + searchParams, { headers });
+  const coursesJSON = await data.json();
+  const courses = coursesJSON.results;
   return {
     props: {
-      title: params.singleCategory.title,
+      category: params.category,
+      courses,
     },
   };
 }
