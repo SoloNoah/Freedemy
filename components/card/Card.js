4b0d5228a3398124daa84c5f10bf18ef02284822
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-09-09 17:17:59 +0300

    Images with lazy load and blur added

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 
 const Card = ({ course }) => {
-  const { headline, title, visible_instructors } = course;
+  const { headline, title, visible_instructors, image_240x135 } = course;
 
   return (
     <div>
       <h1>{title}</h1>
       <p>{headline}</p>
-
+      <Image src={image_240x135} alt={title} blurDataURL={image_240x135} placeholder='blur' width={240} height={135} />
       <ul>
         {visible_instructors.map((instructor) => {
           return <li key={instructor.url}>{instructor.title}</li>;
